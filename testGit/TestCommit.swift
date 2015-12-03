c548aafc7fb73aa51f974c684023763058ed3e20
Xseed <xseed@mac-mini-de-xseed.local>
2015-12-03 15:25:28 -0300

    prueba

---
+++ testGit/TestCommit.swift
@@ -0,0 +1,13 @@
+//
+//  TestCommit.swift
+//  testGit
+//
+//  Created by Xseed on 3/12/15.
+//  Copyright © 2015 Xseed. All rights reserved.
+//
+
+import Cocoa
+
+class TestCommit: UIViewController {
+
+}
